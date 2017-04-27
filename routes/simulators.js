d773d10b566a88cbb0e6709288f74e109c1c734e
Markus Grigull <web@grigull.me>
2017-04-27 15:25:19 +0200

    Add GPLv3 License

--- routes/simulators.js
+++ routes/simulators.js
@@ -2,10 +2,22 @@
  * File: simulators.js
  * Author: Markus Grigull <mgrigull@eonerc.rwth-aachen.de>
  * Date: 28.09.2016
- * Copyright: 2016, Institute for Automation of Complex Power Systems, EONERC
- *   This file is part of VILLASweb. All Rights Reserved. Proprietary and confidential.
- *   Unauthorized copying of this file, via any medium is strictly prohibited.
- **********************************************************************************/
+ *
+ * This file is part of VILLASweb-backend.
+ *
+ * VILLASweb-backend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VILLASweb-backend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VILLASweb-backend. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 
 // include
 var express = require('express');
