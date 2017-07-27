77837adbcce75bb84f075e22791b931f757a144f
Markus Grigull <web@grigull.me>
2017-07-27 10:02:37 +0200

    Remove unneeded simulation model route and model

--- models/simulation.js
+++ models/simulation.js
@@ -19,61 +19,41 @@
  * along with VILLASweb-backend. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
- // include
- var mongoose = require('mongoose');
+// include
+var mongoose = require('mongoose');
 
- var SimulationModel = require('./simulationModel');
- var Project = require('./project');
- var logger = require('../utils/logger');
+var Project = require('./project');
+var logger = require('../utils/logger');
 
- var Schema = mongoose.Schema;
+var Schema = mongoose.Schema;
 
- // simulation model
- var simulationSchema = new Schema({
-   name: { type: String, required: true },
-   running: { type: Boolean, default: false },
-   /*owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },*/
-   /*models: [{ type: Schema.Types.ObjectId, ref: 'SimulationModel', default: [] }],*/
-   models: { type: Array, default: [] },
-   projects: [{ type: Schema.Types.ObjectId, ref: 'Project', default: [] }]
- });
+// simulation model
+var simulationSchema = new Schema({
+  name: { type: String, required: true },
+  running: { type: Boolean, default: false },
+  models: { type: Array, default: [] },
+  projects: [{ type: Schema.Types.ObjectId, ref: 'Project', default: [] }]
+});
 
- simulationSchema.pre('remove', function(callback) {
-   // delete all models belonging to this simulation
-   this.models.forEach(function(id) {
-     SimulationModel.findOne({ _id: id }, function(err, model) {
-       if (err) {
-         logger.error('Unable to find simulation model for id: ' + id, err);
-         return;
-       }
+simulationSchema.pre('remove', function(callback) {
+  // delete all projects belonging to this simulation
+  this.projects.forEach(function(id) {
+    Project.findOne({ _id: id }, function(err, project) {
+      if (err) {
+        logger.error('Unable to find project for id: ' + id, err);
+        return;
+      }
 
-       model.remove(function(err) {
-         if (err) {
-           logger.error('Unable to remove simulation model', { err, model });
-           return;
-         }
-       });
-     });
-   });
+      project.remove(function(err) {
+        if (err) {
+          logger.error('Unable to remove project', { err, project });
+          return;
+        }
+      });
+    });
+  });
 
-   // delete all projects belonging to this simulation
-   this.projects.forEach(function(id) {
-     Project.findOne({ _id: id }, function(err, project) {
-       if (err) {
-         logger.error('Unable to find project for id: ' + id, err);
-         return;
-       }
+  callback();
+});
 
-       project.remove(function(err) {
-         if (err) {
-           logger.error('Unable to remove project', { err, project });
-           return;
-         }
-       });
-     });
-   });
-
-   callback();
- });
-
- module.exports = mongoose.model('Simulation', simulationSchema);
+module.exports = mongoose.model('Simulation', simulationSchema);
